35798e2c689a7c123c00a9dbe0cd71d39b21a482
M4r10 <66843474+mario20403@users.noreply.github.com>
2023-06-04 22:48:48 -0400

    routingv1

---
+++ src/app/app.routing.ts
@@ -0,0 +1,3 @@
+import { RouterModule, Routes } from "@angular/router";
+import { MenuComponent } from "./menu/menu.component";
+const appRoutes = [{ path: "", component: MenuComponent, pathMatch: "full" }];
