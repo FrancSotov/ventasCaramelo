c06a0c57a300f7123944b26a82b8a517d707971c
F5 <134907362+FrancSotov@users.noreply.github.com>
2023-06-04 22:10:24 -0400

    uwu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+  platos: any[] = [
+    { id: 1, nombre: 'Plato 1', precio: 10, imagen: 'ruta/imagen1.jpg' },
+    { id: 2, nombre: 'Plato 2', precio: 15, imagen: 'ruta/imagen2.jpg' },
+    { id: 3, nombre: 'Plato 3', precio: 12, imagen: 'ruta/imagen3.jpg' },
+    // Agrega más platos según sea necesario
+  ];
 
+  carrito: any[] = [];
+
+  agregarAlCarrito(plato: any) {
+    const itemExistente = this.carrito.find(item => item.id === plato.id);
+    if (itemExistente) {
+      itemExistente.cantidad++;
+    } else {
+      this.carrito.push({ ...plato, cantidad: 1 });
+    }
+  }
+
+  eliminarDelCarrito(item: any) {
+    const index = this.carrito.indexOf(item);
+    if (index > -1) {
+      this.carrito.splice(index, 1);
+    }
+  }
+
+  calcularTotal(): number {
+    let total = 0;
+    for (const item of this.carrito) {
+      total += item.precio * item.cantidad;
+    }
+    return total;
+  }
 }
