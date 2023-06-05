b675154b7b48ef1a2fbac82c176702eb1c8fb803
M4r10 <66843474+mario20403@users.noreply.github.com>
2023-06-05 00:39:45 -0400

    paypal v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+declare var paypal;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+  @ViewChild('paypal',{static:true}) paypalElement : ElementRef;
+
+  producto={
+    descripcion:'venta',
+    precio : 100000
+  }
   title = 'ventasBar';
+
+  ngOnInit() {
+    paypal
+    .Buttons()
+    .render(this.paypalElement.nativeElement);
+  }
 }
